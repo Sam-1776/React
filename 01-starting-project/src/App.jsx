1966dc6802c1e9aa2ca0393b52af3dbdf371d1bf
Samuele Simeone <69748013+Sam-1776@users.noreply.github.com>
2024-04-09 10:21:48 +0200

    add dinamic content

--- 01-starting-project/src/App.jsx
+++ 01-starting-project/src/App.jsx
@@ -1,10 +1,21 @@
+import reactImg from './assets/react-core-concepts.png';
+
+const reactDescription = ['Fundamental', 'Crucial', 'Core'];
+
+function randomNum(max) {
+  return Math.floor(Math.random() * (max + 1));
+}
+
+
 function Header() {
+  const firstW = reactDescription[randomNum(reactDescription.length-1)];
+
   return (
     <header>
-      <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+      <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        Fundamental React concepts you will need for almost any app you are
+        {firstW} React concepts you will need for almost any app you are
         going to build!
       </p>
     </header>
